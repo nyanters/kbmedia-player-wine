61f745bd1e2a365a2f1e3bfadc86563edbf9b6b5
nyanters <123231885+nyanters@users.noreply.github.com>
2025-06-09 09:17:15 +0900

    modified:   .bin/darwin.bash

--- .bin/darwin.bash
+++ .bin/darwin.bash
@@ -3,7 +3,7 @@ set -euxo pipefail
 SCR_DIR=$(cd "$(dirname "$0")"; pwd)
 cd "${SCR_DIR}"
 # Check operating system
-if [ "$(uname)" != "Darwin" ] ; then
+if [[ "$(uname)" != "Darwin" ]] ; then
   echo "Not macOS!"
   exit 1
 fi
